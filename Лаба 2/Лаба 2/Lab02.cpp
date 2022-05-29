77eb0cc36191fb7c4cde5bdf52f64b0ea82dcfb0
XoJIoDuJIHuK <91029172+XoJIoDuJIHuK@users.noreply.github.com>
2022-05-30 00:22:19 +0300

    Add files via upload

---
+++ Лаба 2/Лаба 2/Lab02.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int max_number_of_steps = 0;
+int N, middle, new_N;
+
+int max_steps()
+{
+    while (new_N != 1)
+    {
+        cout << new_N << endl;
+        if (new_N % 2 == 0)
+            new_N = new_N / 2;
+        else
+            new_N = new_N / 2 + 1;
+        max_number_of_steps++;
+    }
+    return max_number_of_steps++;
+}
+
+int main()
+{
+    system("chcp 1251 > nul");
+    int left = 1;
+    string answer, question;
+    do
+    {
+        cout << "Введите верхний порог: ";
+        cin >> N;
+        new_N = N;
+
+        while (N - left)
+        {
+            int middle = left + (N - left) / 2;
+            cout << middle << " - мало / много / угадал" << endl;
+            cin >> answer;
+            if (answer == "мало")
+            {
+                left = middle + 1;
+            }
+            else if (answer == "много")
+            {
+                N = middle;
+            }
+            else if (answer == "угадал")
+            {
+                cout << "Вы загадали число " << middle << endl;
+                break;
+            }
+        }
+        cout << "Максимальное количество шагов: " << max_steps() << endl;
+
+        cout << "Хотите сыграть еще? (д или н)?\n";
+        cin >> question;
+        system("pause");
+    } while (question == "Д" || question == "д");
+}
